b7f98f078f204bcc31929a6870513587904410e0
danielecioffo <d.cioffo@studenti.unipi.it>
2021-05-13 21:37:36 +0200

    Test done

--- Antagonist/src/main.py
+++ Antagonist/src/main.py
@@ -3,7 +3,7 @@ import docker
 if __name__ == '__main__':
     client = docker.from_env()
     list_of_containers = client.containers.list()
-    print('List of containers: ' + list_of_containers + '\n')
     for container in list_of_containers:
         print('Container ID: ' + container.id + '\n')
-        container.restart()
+    list_of_images = client.images.list()
+    print(list_of_images)
