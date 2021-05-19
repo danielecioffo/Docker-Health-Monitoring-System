ddee1750962a3eb4e45f400f6aeec66c60b4968e
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-05-19 17:48:08 +0200

    communication module

--- Agent/communication.py
+++ Agent/communication.py
@@ -1,14 +1,20 @@
 # consume messages from REST interface
 import pika
+import socket
+import agent
 
 
-def change_treshold(val):
-    print('Hello')
-    # change threshold
+def change_threshold(val):
+    agent.change_threshold(val)
 
 
 def activate_deactivate_container(container_name, hostname, new_status):
-    print('Hello')
+    if hostname != socket.gethostname():
+        return
+    if new_status == 'True':
+        agent.add_to_monitored(container_name)
+    elif new_status == 'False':
+        agent.remove_from_monitored(container_name)
     # change the status of the selected container
 
 
@@ -19,6 +25,8 @@ def provide_list_of_containers():
     """
     string = ''  # find status of containers
 
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.16.3.172'))  # broker ip address
+    channel = connection.channel()
     channel.queue_declare(queue='list_response')
     channel.basic_publish(exchange='', routing_key='list_response', body=string)
     connection.close()
@@ -28,18 +36,27 @@ def generic_callback(ch, method, properties, body):
     print("Hello, message received")
     # parse message, call the correct method
     # if topic is threshold, call change threshold
+    if method.routing_key == 'threshold':
+        change_threshold(body.decode())
     # if topic is list_request, call provide_list_of_containers
+    elif method.routing_key == 'list_request':
+        provide_list_of_containers()
     # if topic is actives, call activate_deactivate_container
+    elif method.routing_key == 'actives':
+        body_string = body.decode().replace("(", "").replace(")", "")
+        body_tuple = tuple(map(str, body_string))
+        activate_deactivate_container(body_tuple[0], body_tuple[1], body_tuple[2])
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.16.3.172'))  # broker ip address
-channel = connection.channel()
-channel.exchange_declare(exchange='topics', exchange_type='topic')
-result = channel.queue_declare('', exclusive=True)
-queue_name = result.method.queue
-channel.queue_bind(exchange='topics', queue=queue_name, routing_key='threshold')
-channel.queue_bind(exchange='topics', queue=queue_name, routing_key='list_request')
-channel.queue_bind(exchange='topics', queue=queue_name, routing_key='actives')
+def initialize_communication():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.16.3.172'))  # broker ip address
+    channel = connection.channel()
+    channel.exchange_declare(exchange='topics', exchange_type='topic')
+    result = channel.queue_declare('', exclusive=True)
+    queue_name = result.method.queue
+    channel.queue_bind(exchange='topics', queue=queue_name, routing_key='threshold')
+    channel.queue_bind(exchange='topics', queue=queue_name, routing_key='list_request')
+    channel.queue_bind(exchange='topics', queue=queue_name, routing_key='actives')
 
-channel.basic_consume(queue=queue_name, on_message_callback=generic_callback, auto_ack=True)
-channel.start_consuming()
+    channel.basic_consume(queue=queue_name, on_message_callback=generic_callback, auto_ack=True)
+    channel.start_consuming()
