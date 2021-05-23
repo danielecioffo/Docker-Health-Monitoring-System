1f22aa433767bc1257b4a0c1cee2a9df13ef32b8
edofazza <fazzari.edoardo@gmail.com>
2021-05-23 09:36:19 +0200

    REST description modification

--- Rest/swagger_server/controllers/threshold_controller.py
+++ Rest/swagger_server/controllers/threshold_controller.py
@@ -7,7 +7,7 @@ from swagger_server import util
 def put_threshold(thresholdValue):  # noqa: E501
     """Update packet loss threshold
 
-    Update packet loss threshold used in the Agents # noqa: E501
+    Update packet loss threshold used in the Agent # noqa: E501
 
     :param thresholdValue: New value threshold
     :type thresholdValue: float
