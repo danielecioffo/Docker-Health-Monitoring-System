e4e207bb9108880fc3308b35c0c380f012957d62
danielecioffo <d.cioffo@studenti.unipi.it>
2021-05-25 16:37:15 +0200

    Antagonist improved

--- Antagonist/antagonist.py
+++ Antagonist/antagonist.py
@@ -4,22 +4,29 @@ import logging
 from datetime import datetime
 import time
 
-NAME_OF_ANTAGONIST_CONTAINER = "antagonist"
-NAME_OF_AGENT_CONTAINER = "agent"
 INTERVAL_BETWEEN_ATTACKS = 30  # seconds
+
+CONTAINERS_TO_AVOID = [
+    "antagonist",
+    "rabbitMq-broker",
+    "swagger_server"
+]
 
 
 def periodic_attack():
     list_of_containers = client.containers.list()  # get the active containers
-    list_of_containers.remove(client.containers.get(NAME_OF_ANTAGONIST_CONTAINER))  # remove myself from the list
+    for name in CONTAINERS_TO_AVOID:
+        try:
+            list_of_containers.remove(client.containers.get(name))
+        except:
+            pass
     if len(list_of_containers) != 0:  # if there are others containers active
         # STOP ONE CONTAINER
         index = random.randrange(len(list_of_containers))  # random value from 0 to length (of the list of containers)
         container_to_stop = list_of_containers[index]
         # stop the chosen container
-        if container_to_stop.name != NAME_OF_AGENT_CONTAINER:  # do not consider the agent in attacks
-            logging.info("%s - Container stopped: %s\n", datetime.now(), container_to_stop.name)
-            container_to_stop.stop()
+        logging.info("%s - Container stopped: %s\n", datetime.now(), container_to_stop.name)
+        container_to_stop.stop()
 
 
 if __name__ == '__main__':
